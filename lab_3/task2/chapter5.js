bd9960766d52cd6e88ae824f8eb06493b4e92f7f
dilya <dilnarauzakbayeva140@gmail.com>
2026-02-08 03:47:37 +0500

    a

--- lab_3/task2/chapter5.js
+++ lab_3/task2/chapter5.js
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html>
+<body>
+    <script>
+        let a=5, b=2;
+        if (a + b < 4) {
+        result = 'Мало';
+        } else {
+        result = 'Много';
+        }
+    </script>
+</body>
+</html>
