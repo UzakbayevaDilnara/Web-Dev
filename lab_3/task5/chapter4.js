38b14f616afd4ca45a16a0d2a8f2a4ede6182c82
dilya <dilnarauzakbayeva140@gmail.com>
2026-02-08 04:16:07 +0500

    a

---
+++ lab_3/task5/chapter4.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <script>
+    let arr = ["HTML", "JavaScript", "CSS"];
+    let sorted = copySorted(arr);
+    alert( sorted ); 
+    alert( arr ); 
+    </script>
+</body>
+</html>
+
