deeb033317f3ba394a5d5f4038c0852c5e124ce2
dilya <dilnarauzakbayeva140@gmail.com>
2026-02-08 03:07:37 +0500

    ф

--- lab_3/task2/chapter4.js
+++ lab_3/task2/chapter4.js
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html>
+<body>
+    <script>
+        let a = 1, b = 1;
+        let c = ++a; // 2 жаңасын қайтаратын иооо
+        let d = b++; // 1 бастапқы мәнін қайтарады
+    </script>
+</body>
+</html>
