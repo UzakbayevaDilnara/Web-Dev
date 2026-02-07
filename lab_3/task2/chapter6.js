7d9c258fa69cb91af9f0fffa34a7879317cacf68
dilya <dilnarauzakbayeva140@gmail.com>
2026-02-08 03:50:22 +0500

    a

---
+++ lab_3/task2/chapter6.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <script>
+    let num1 = 10,
+    num2 = 20,
+    result;
+
+    if (result === null || result === undefined) {
+    if (num1 !== null && num1 !== undefined) {
+    result = num1;
+  } else {
+    result = num2;
+  }
+}    
+      </script>
+</body>
+</html>
