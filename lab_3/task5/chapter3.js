963ffb856457b6d4d2b57b74821d04079abccdca
dilya <dilnarauzakbayeva140@gmail.com>
2026-02-08 04:14:27 +0500

    a

---
+++ lab_3/task5/chapter3.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <script>
+  
+    let fruits = ["Яблоки", "Груша", "Апельсин"];
+    let shoppingCart = fruits;
+    shoppingCart.push("Банан");
+
+    alert( fruits.length ); 
+    </script>
+</body>
+</html>
