ef36439210c41ed61e3050b3b433e56eb4a74c4b
dilya <dilnarauzakbayeva140@gmail.com>
2026-03-17 23:36:27 +0500

    Update main.py

--- lab7/task2/main.py
+++ lab7/task2/main.py
@@ -1,1 +1,16 @@
+from models import Car, Bike
 
+def main():
+    car1 = Car("Toyota", 2020, 120, 4)
+    bike1 = Bike("BMX", 2022, 40, "sport")
+
+    vehicles = [car1, bike1]
+
+    for v in vehicles:
+        print(v)              
+        print(v.move())      
+        print(v.stop())
+        print("-----")
+
+if __name__ == "__main__":
+    main()
