1810f8cc2ee8aac2c80698f2e52cb0b46d097a0a
dilya <dilnarauzakbayeva140@gmail.com>
2026-03-26 00:18:25 +0500

    Update manage.py

--- lab8/shop_back/manage.py
+++ lab8/shop_back/manage.py
@@ -1,1 +1,8 @@
-python manage.py makemigrations
+#!/usr/bin/env python
+import os
+import sys
+
+if __name__ == "__main__":
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_back.settings')
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
